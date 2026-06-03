e859591b919825990ffa0f2755cb4b56d7098bec
Typebase Dev <typebase-dev@typebase.io>
2026-06-03 09:40:09 -0400

    fix(core/auth): infer auth options correctly

--- apps/core/src/server/auth/index.ts
+++ apps/core/src/server/auth/index.ts
@@ -1,8 +1,8 @@
-import { type BetterAuthOptions, betterAuth } from 'better-auth';
+import { type Auth, type BetterAuthOptions, betterAuth } from 'better-auth';
 import { APIError } from 'better-auth/api';
 
 export { createAuthMiddleware } from 'better-auth/api';
 
-export const defineAuth = (options: Omit<BetterAuthOptions, 'database'>) => betterAuth(options);
+export const defineAuth = <Options extends Omit<BetterAuthOptions, 'database'>>(options: Options): Auth<Options> => betterAuth(options);
 
 export const AuthError = APIError;
