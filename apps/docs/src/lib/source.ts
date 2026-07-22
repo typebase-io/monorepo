2d9fae104d5e8be9190d59db0d3702d063a5f98f
Typebase Dev <typebase-dev@typebase.io>
2026-07-22 08:59:52 -0400

    feat(apps/docs): update deps

--- apps/docs/src/lib/source.ts
+++ apps/docs/src/lib/source.ts
@@ -1,7 +1,7 @@
 import { type InferPageType, loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
 
-import { docs } from '#fumadocs-mdx:collections/server.ts';
+import { docs } from '#collections/server.ts';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
