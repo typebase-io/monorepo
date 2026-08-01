100e1e5c83db3f42e7f09c3b044bd133064c0291
Typebase Dev <typebase-dev@typebase.io>
2026-08-01 16:10:12 -0400

    feat(apps/examples): use last version of typebase code

--- apps/examples/nextjs/app-router/src/lib/typebase/server/_generated/server.ts
+++ apps/examples/nextjs/app-router/src/lib/typebase/server/_generated/server.ts
@@ -1,20 +1,26 @@
 // ⚠️ AUTO-GENERATED FILE — DO NOT EDIT
 
-import type { ActionBuilder, GetDBBuilder } from "typebase-io/server";
-import type { relations } from "../db/relations.ts";
+import type { ActionBuilder, GetDBBuilder, InferRouterInputs, InferRouterOutputs } from 'typebase-io/server';
+import type { env as envSchema } from '../env.ts';
+import type { relations } from '../db/relations.ts';
 
-import * as MutationsTodos from "../actions/mutations/todos.ts";
-import * as QueriesTodos from "../actions/queries/todos.ts";
+import { filterActions } from 'typebase-io/server';
+
+import * as ActionModule0 from '../actions/mutations/todos.ts';
+import * as ActionModule1 from '../actions/queries/todos.ts';
 
 export const router = {
   mutations: {
-    todos: MutationsTodos,
+    todos: filterActions(ActionModule0),
   },
   queries: {
-    todos: QueriesTodos,
+    todos: filterActions(ActionModule1),
   },
 };
 
 export type Router = typeof router;
-export declare const action: ActionBuilder<typeof relations>;
+export type RouterInputs = InferRouterInputs<typeof router>;
+export type RouterOutputs = InferRouterOutputs<typeof router>;
+
+export declare const action: ActionBuilder<typeof relations, never, typeof envSchema>;
 export declare const getDB: GetDBBuilder<typeof relations>;
